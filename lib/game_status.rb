d004f1dd5b0554a598ad2632605a94140113d761
Daniel Costelloe <danny.costelloe@gmail.com>
2021-01-21 14:55:54 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
-  [6,7,8]  # Bottom row
+  [6,7,8],  # Bottom row
   [0,3,6], # left column
   [1,4,7], # middle column
   [2,5,8], # right columm
